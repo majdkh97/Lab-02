232cf1fca031188bacfedd08c06cf478b45fe6f1
majdkh97 <majd.motaz@yahoo.com>
2021-04-05 14:45:27 +0300

    final touches

--- js/app.js
+++ js/app.js
@@ -1,51 +1,51 @@
 'use strict';
 
-let userName = prompt('What\'s your name ')
+let userName = prompt('What\'s your name ');
 console.log(userName);
 alert('Welcome to my website ' + userName);
-alert('Am gonna ask you five questions please answer by yes/y or no/n')
+alert('Am gonna ask you five questions please answer by yes/y or no/n');
 
 function userChoice(question)
 {
-    while(question !== 'yes' && question !== 'y' && question !== 'no' && question !== 'n')
-    {
-        question = prompt('please answer with either yes/y or no/n');
-    }
-    return question;
+  while(question !== 'yes' && question !== 'y' && question !== 'no' && question !== 'n')
+  {
+    question = prompt('please answer with either yes/y or no/n');
+  }
+  return question;
 }
 
 function mySwitch(question)
-{   
-    switch(question)
-        {
-            case 'yes':
-            case 'y'  :
-                alert('yes, i actually do');
-                break;
-            case 'no':
-            case 'n'  :
-                alert('no, i actually do');
-                break;
-            default:
-            alert('please answer with either yes/y or no/n');     
-        }
+{
+  switch(question)
+  {
+  case 'yes':
+  case 'y' :
+    alert('yes, i actually do');
+    break;
+  case 'no':
+  case 'n' :
+    alert('no, i actually do');
+    break;
+  default:
+    alert('please answer with either yes/y or no/n');
+  }
 }
 
 function mySwitch1(question)
-{   
-    switch(question)
-        {
-            case 'yes':
-            case 'y'  :
-                alert('no, i actually don\'t');
-                break;
-            case 'no':
-            case 'n'  :
-                alert('yes, i actually don\'t');
-                break;
-            default:
-            alert('please answer with either yes/y or no/n');     
-        }
+{
+  switch(question)
+  {
+  case 'yes':
+  case 'y' :
+    alert('no, i actually don\'t');
+    break;
+  case 'no':
+  case 'n' :
+    alert('yes, i actually don\'t');
+    break;
+  default:
+    alert('please answer with either yes/y or no/n');
+  }
 }
 
 
@@ -53,7 +53,7 @@ let question1 = prompt('Do i like flafel');
 console.log(question1);
 mySwitch(userChoice(question1.toLowerCase()));
 
-let question2 = prompt('Do i like hummus')
+let question2 = prompt('Do i like hummus');
 console.log(question2);
 mySwitch(userChoice(question2.toLowerCase()));
 
@@ -69,3 +69,4 @@ let question5 = prompt('Do i like mutton');
 console.log(question5);
 mySwitch1(userChoice(question5.toLowerCase()));
 
+alert('Thank you for visting my website Mr/Ms ' + userName);
